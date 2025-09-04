4d7c4bde8988783dc5fedd5c1c25009a40bc633e
snicker <ll3925430@gmai.com>
2025-09-04 17:09:42 -0500

    actualizacion

---
+++ java/Taller6/SumarHastaN.java
@@ -0,0 +1,28 @@
+package Taller6;
+
+import java.util.Scanner;
+
+public class SumarHastaN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Pedir N
+        System.out.print("Ingrese un valor para N: ");
+        int n = scanner.nextInt();
+       
+        String numeroTexto = "";
+       
+        int suma = 0 ;
+
+       for (int i = 1; i <= n; i++) {
+        suma += i;
+        if (i < n) {
+            numeroTexto += i + " + ";
+        }else {
+            numeroTexto += i;
+        }
+       }
+       System.out.println(numeroTexto + " = " + suma);
+       scanner.close();
+    }   
+}
---
+++ java/Taller6/sumadeparesedeN.java
@@ -0,0 +1,20 @@
+package Taller6;
+
+import java.util.Scanner;
+
+public class sumadeparesedeN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un valor para N");
+        int n = scanner.nextInt();
+
+        int suma = 0;
+
+        for (int i = 2; i <= n; i += 2) {
+            
+        }
+
+
+    }
+}
